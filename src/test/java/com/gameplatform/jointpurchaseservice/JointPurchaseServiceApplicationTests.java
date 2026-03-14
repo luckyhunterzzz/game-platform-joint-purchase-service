b4087101ac0738e5bd3a449e95ca25e266c4d396
Airat Zaripov <lucky_hunter@list.ru>
2026-03-14 14:13:29 +0100

    feature/GPJPS-1 - Initial commit: added Gradle project structure, basic dependencies

---
+++ src/test/java/com/gameplatform/jointpurchaseservice/JointPurchaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gameplatform.jointpurchaseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JointPurchaseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
